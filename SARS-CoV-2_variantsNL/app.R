292da3ef2dab9c32ae1f172bce5edcf71278602f
Caspar Verhey <caspar@verhey.net>
2021-07-16 22:53:54 +0200

    link naar GitHub

--- SARS-CoV-2_variantsNL/app.R
+++ SARS-CoV-2_variantsNL/app.R
@@ -102,7 +102,7 @@ ui <- fluidPage(
                          Het is ook belangrijk op te merken dat deze cijfers alleen de geteste personen betreffen, en dus niet alle geïnfecteerden. Het RIVM maakt voorlopig ook <a href='https://data.rivm.nl/meta/srv/dut/catalog.search#/metadata/097155aa-75eb-4caa-8ed3-4c6edb80467e'>geen schattingen meer over het aantal besmettelijke personen</a>.<br />
                          <br />
                          Databronnen: RIVM open data <a href='https://www.rivm.nl/coronavirus-covid-19/virus/varianten'>kiemsurveillance</a> en <a href='https://data.rivm.nl/geonetwork/srv/dut/catalog.search#/metadata/1c0fcd57-1102-4620-9cfa-441e93ea5604?tab=general'>infectierapportage</a><br />
-                         Verwerking: <a href='http://github.com/cwverhey/'>CW Verhey</a> (caspar @ verhey.net)"))
+                         Verwerking: CW Verhey (caspar @ verhey.net) <a href='https://github.com/cwverhey/COVID-19/tree/master/SARS-CoV-2_variantsNL'>(broncode op GitHub)</a>"))
             ))
     )
 )
