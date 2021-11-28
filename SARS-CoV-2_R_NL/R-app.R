fff4c90cfb9d5f35abac17616555eab959441b5b
Caspar Verhey <caspar@verhey.net>
2021-11-28 22:38:13 +0100

    update: split app and cache prepare into 2 files

--- SARS-CoV-2_R_NL/R-app.R
+++ SARS-CoV-2_R_NL/R-app.R
@@ -5,60 +5,10 @@ library("shinyjs")
 library("ggplot2")
 library("scales")
 
-# source files need to be in <jsondir>/reproductiegetal_*.json
-jsondir = '/Volumes/caspar/RIVM'
-
-# remove cache file to update
-cachefile = 'R.app.cache.RData'
-
 Sys.setlocale("LC_TIME","nl_NL.UTF-8")
 
-if(!file.exists(cachefile)) {
-  
-  # load JSONs retrieved every day from https://data.rivm.nl/meta/srv/dut/catalog.search#/metadata/ed0699d1-c9d5-4436-8517-27eb993eab6e
-  file.ls <- list.files(path=jsondir, pattern=glob2rx("reproductiegetal_*.json"), full.names=T)
-  
-  if(length(file.ls) == 0) stop("Error: no input files!")
-  
-  # get data
-  df <- data.frame()
-  lastdf <- 0
-  for(f in file.ls) {
-    print(f)
-    date <- gsub('.*(\\d{4}-\\d{2}-\\d{2}).*', '\\1', f)
-    print(date)
-    df.temp <- jsonlite::read_json(f, simplifyDataFrame=T)
-    df.temp$Rt_low <- as.numeric(df.temp$Rt_low)
-    df.temp$Rt_avg <- as.numeric(df.temp$Rt_avg)
-    df.temp$Rt_up  <- as.numeric(df.temp$Rt_up)
-    df.temp$source <- as.Date(date)
-    if(nrow(df.temp) != lastdf) {
-      df <- bind_rows(df, df.temp)
-      lastdf <- nrow(df.temp)
-    }
-  }
-  rm(f, date, df.temp)
-  
-  df %>%
-    select(source, Date, Rt_low, Rt_avg, Rt_up) %>%
-    rename(date=Date) %>%
-    mutate(source = as.Date(source), date = as.Date(date)) -> df
-  
-  print(head(df))
-  
-  # list unique source / dates
-  all.sources = unique(df$source)
-  all.dates = unique(df$date)
-  
-  # disease mitigation dates
-  # https://nl.wikipedia.org/wiki/Maatregelen_tijdens_de_coronacrisis_in_Nederland
-  mitigation.dates = c("2020-10-14", "2020-11-03", "2020-12-14", "2021-01-20", "2021-04-20", "2021-05-11", "2021-06-05", "2021-06-26", "2021-07-09")
-  
-  save(df, colors, all.sources, all.dates, mitigation.dates, file="R.app.cache.RData")
-  print("saved")
-
-}
-
+# cache file is created by prepare-cache.R
+cachefile = 'R.app.cache.RData'
 load(cachefile)
 print("loaded")
 
